0dd43feb912cf2541abc60a49200c1856378df2f
miplo <miplotnova@mail.ru>
2024-12-13 21:10:59 +0500

    Proect3

---
+++ src/api.py
@@ -0,0 +1,22 @@
+import requests
+
+
+class HHAPI:
+    BASE_URL = 'https://api.hh.ru'
+
+    @staticmethod
+    def get_companies(companies):
+        companies_data = []
+        for company in companies:
+            response = requests.get(f"{HHAPI.BASE_URL}/employers/{company}")
+            if response.status_code == 200:
+                companies_data.append(response.json())
+        return companies_data
+
+    @staticmethod
+    def get_vacancies(company_id):
+        vacancies = []
+        response = requests.get(f"{HHAPI.BASE_URL}/vacancies?employer_id={company_id}")
+        if response.status_code == 200:
+            vacancies = response.json().get('items', [])
+        return vacancies
